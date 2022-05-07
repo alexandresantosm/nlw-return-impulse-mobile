b92b614d99c88c0c807dd45c8a2ff550654a9152
Alexandre Menezes <amenezes031@gmail.com>
2022-05-07 12:06:17 -0300

    feat: create feedback types

---
+++ src/utils/feedbackTypes.ts
@@ -0,0 +1,14 @@
+export const feedbackTypes = {
+  'BUG': {
+    title: '',
+    image: require('../assets/bug.png'),
+  },
+  'IDEIA': {
+    title: '',
+    image: require('../assets/idea.png'),
+  },
+  'OTHER': {
+    title: '',
+    image: require('../assets/other.png'),
+  },
+};
