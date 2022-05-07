3708a882c091637c3b3be9ac768f3bae53c76683
Alexandre Menezes <amenezes031@gmail.com>
2022-05-07 19:08:28 -0300

    feat: create Form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,62 @@
+import { ArrowLeft, X } from 'phosphor-react-native';
+import React from 'react';
+import {
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+import { FeedbackType, feedbackTypes } from '../../utils/feedbackTypes';
+
+import { theme } from '../../theme';
+import { styles } from './styles';
+
+interface FormProps {
+  feedbackType: FeedbackType;
+}
+
+export const Form: React.FC<FormProps> = ({ feedbackType }: FormProps) => {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+      <TouchableOpacity>
+        <ArrowLeft
+          size={24}
+          weight='bold'
+          color={theme.colors.text_secondary}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.titleContainer}>
+        <Image
+          source={feedbackTypeInfo.image}
+          style={styles.image}
+        />
+        
+        <Text style={styles.titleText}>
+          {feedbackTypeInfo.title}
+        </Text>
+      </View>
+
+      <TouchableOpacity>
+        <X
+          size={24}
+          weight='bold'
+          color={theme.colors.text_secondary}
+        />
+      </TouchableOpacity>
+    </View>
+
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder='Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo...'
+        placeholderTextColor={theme.colors.text_secondary}
+      />
+    </View>
+  );
+}
