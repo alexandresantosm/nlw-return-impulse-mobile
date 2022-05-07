f91ba8bcb0116aa325949b9e8474f8fdb70ea844
Alexandre Menezes <amenezes031@gmail.com>
2022-05-07 12:32:07 -0300

    feat: create widget component

---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+export const Widget: React.FC = () => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
