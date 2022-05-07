d2e39a1eb9f8ec2b61cccc42e76bca6e8261cc0c
Alexandre Menezes <amenezes031@gmail.com>
2022-05-07 15:08:21 -0300

    feat: create feedback modal

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
 
-export const Widget: React.FC = () => {
+const WidgetComponent: React.FC = () => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const handleOpenFeedbackMenu = () => {
+    bottomSheetRef.current?.expand();
+  }
+
   return (
     <>
       <TouchableOpacity
         style={styles.button}
+        onPress={handleOpenFeedbackMenu}
       >
           <ChatTeardropDots
             size={24}
@@ -17,6 +26,17 @@ export const Widget: React.FC = () => {
             color={theme.colors.text_on_brand_color}
           />
       </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+
+      </BottomSheet>
     </>
   );
 }
+
+export const Widget = gestureHandlerRootHOC(WidgetComponent);
