c3e4b8abe86ac20d82b5f08802c6b797a4563d30
Alexandre Menezes <amenezes031@gmail.com>
2022-05-07 20:37:45 -0300

    feat: create button send feedback

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ActivityIndicator
+} from 'react-native';
+import { theme } from '../../theme';
+
+import { styles } from './styles';
+
+interface ButtonProps extends TouchableOpacityProps {
+  isLoading: boolean;
+}
+
+export const Button: React.FC<ButtonProps> = ({ isLoading, ...rest }: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      {...rest}
+    >
+      {
+        isLoading
+        ?
+          <ActivityIndicator
+            color={theme.colors.text_on_brand_color}
+          />
+        :
+          <Text
+            style={styles.title}
+          >
+            Enviar feedback
+          </Text>
+      }
+    </TouchableOpacity>
+  );
+}
