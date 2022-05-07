fa717907ce35093a221cccca2c9a367b08db0044
Alexandre Menezes <amenezes031@gmail.com>
2022-05-07 16:00:26 -0300

    feat: add option componet in the options componets

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Copyright } from '../Copyright';
 
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
 import { styles } from './styles';
+import { Option } from '../Option';
 
 export const Options: React.FC = () => {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>
+        Deixe seu feedback
+      </Text>
+
+      <View style={styles.options}>
+        {
+          Object
+            .entries(feedbackTypes)
+              .map(([key, value], index) => (
+                <Option
+                  key={`${key}-${index}`}
+                  title={value.title}
+                  image={value.image}
+                />
+              ))
+        }
+      </View>
+
       <Copyright />
     </View>
   );
